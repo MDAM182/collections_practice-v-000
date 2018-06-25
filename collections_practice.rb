154ceaab5bb285d38549c12f200ebf16eb861257
Michael Morris <mdam2345@gmail.com>
2018-06-25 01:34:11 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,21 @@
+def sort_array_asc(numbers)
+  numbers.sort do |a,b|
+     a <=> b
+   end
+end
 
+def sort_array_desc(numbers)
+  numbers.sort.reverse do |b,a|
+      b <=> a
+    end
+end
+
+def sort_array_char_count(animals)
+  animals.sort do |a,b|
+      a.length <=> b.length
+    end
+end
+
+def swap_elements(array,element)
+
+end
