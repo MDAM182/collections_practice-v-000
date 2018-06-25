aa9a05ae1c05e5ccf54c19c1bd8327caed72ac02
Michael Morris <mdam2345@gmail.com>
2018-06-25 03:38:24 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -46,8 +46,7 @@ end
 
 def add_s(elements)
   elements.each_with_index.collect do |element, index|
-    elements << "s" unless feet 
-    false
-  
+    elements << "s" unless feet false
+
   end
 end
